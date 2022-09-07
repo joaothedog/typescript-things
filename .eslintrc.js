310cdcd198822f990c4d45cffc8f023ddbb6b1d5
João Oliveira <jvcbatist4@outlook.com>
2022-09-07 19:33:07 -0300

    Configurados os primeiros arquivos para ambiente de desenvolvimento

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'no-console': 1, // Means warning
+    'prettier/prettier': 2, // Means error
+  },
+};
