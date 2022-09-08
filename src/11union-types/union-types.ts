4647aaf12423f3f4b91e44e4e3911e20d343a96c
João Oliveira <jvcbatist4@outlook.com>
2022-09-08 09:29:19 -0300

    union-types

---
+++ src/11union-types/union-types.ts
@@ -0,0 +1,12 @@
+export function sumOrConcat(
+  a: number | string,
+  b: number | string,
+): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(sumOrConcat(10, 20));
+console.log(sumOrConcat('10', '20'));
+console.log(sumOrConcat(10, '20'));
+console.log(sumOrConcat('10', 20));
